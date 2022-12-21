add0db2cee8034258f2a74632dbd0cfa97297fe7
Íñigo Aréjula Aísa <arejula27@gmail.com>
2022-12-22 00:32:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@
 [[ -f ~/.zshrc ]] && mv .~/.zshrc ~/.zshrc.old
 cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
+
+[[ -f ~/.config/gtk4.0 ]] && mv ~/.config/gtk4.0  ~/.config/gtk4.0.old
+cp -r ./gtk4.0  ~/.config/gtk4.0 
+[[ -f ~/.alacritty.yml ]] && mv .~/.alacritty.yml ~/.alacritty.yml.old
+cp ./.alacritty.yml ~/.alacritty.yml
